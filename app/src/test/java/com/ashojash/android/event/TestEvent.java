5491f55d8dbc12276cd16d56e819d57904b25297
Mehrdad Shokri <shokri_m@hotmail.com>
2016-06-12 22:35:45 +0430

    WIP

---
+++ app/src/test/java/com/ashojash/android/event/TestEvent.java
@@ -0,0 +1,5 @@
+package com.ashojash.android.event;
+
+public class TestEvent {
+
+}
---
+++ app/src/test/java/com/ashojash/android/model/TestReview.java
@@ -0,0 +1,11 @@
+package com.ashojash.android.model;
+
+import org.junit.Test;
+
+public class TestReview {
+
+    @Test
+    public void hello_world_test() {
+
+    }
+}
