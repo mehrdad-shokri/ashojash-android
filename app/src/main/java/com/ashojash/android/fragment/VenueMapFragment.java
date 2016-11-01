a7a2bace1a05599746259583a9b27cddc72263e2
Mehrdad Shokri <shokri_m@hotmail.com>
2016-11-01 21:33:37 +0330

    Update play services

--- app/src/main/java/com/ashojash/android/fragment/VenueMapFragment.java
+++ app/src/main/java/com/ashojash/android/fragment/VenueMapFragment.java
@@ -13,6 +13,7 @@ import com.ashojash.android.helper.AppController;
 import com.ashojash.android.model.Venue;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -22,70 +23,67 @@ import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
 
 public class VenueMapFragment extends Fragment {
-    private LatLng venueLatLng;
-    private Venue venue;
+  private LatLng venueLatLng;
+  private Venue venue;
 
-    public VenueMapFragment() {
-    }
+  public VenueMapFragment() {
+  }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_venue_map, container, false);
-    }
+  @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_venue_map, container, false);
+  }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+  @Override public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    setupViews();
+    setupVenueMap();
+    String slug = getActivity().getIntent().getStringExtra("slug");
+    if (slug == null) getActivity().getFragmentManager().popBackStack();
+    venue =
+        AppController.gson.fromJson(getActivity().getIntent().getStringExtra("venue"), Venue.class);
+    venueLatLng = new LatLng(venue.location.lat, venue.location.lng);
+  }
+
+  private SupportMapFragment mMapFragment;
+
+  private void setupVenueMap() {
+    final String TAG = AppController.TAG;
+    mMapFragment = new SupportMapFragment() {
+      @Override public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        setupViews();
-        setupVenueMap();
-        String slug = getActivity().getIntent().getStringExtra("slug");
-        if (slug == null)
-            getActivity().getFragmentManager().popBackStack();
-        venue = AppController.gson.fromJson(getActivity().getIntent().getStringExtra("venue"), Venue.class);
-        venueLatLng = new LatLng(venue.location.lat, venue.location.lng);
-    }
-
-    private SupportMapFragment mMapFragment;
-    private GoogleMap mMap;
-
-    private void setupVenueMap() {
-        final String TAG = AppController.TAG;
-        mMapFragment = new SupportMapFragment() {
-            @Override
-            public void onActivityCreated(Bundle savedInstanceState) {
-                super.onActivityCreated(savedInstanceState);
-                mMap = mMapFragment.getMap();
-                if (mMap != null) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                    Marker hamburg = mMap.addMarker(new MarkerOptions().position(venueLatLng)
-                            .icon(BitmapDescriptorFactory.fromBitmap(new IconicsDrawable(AppController.context)
-                                    .icon(GoogleMaterial.Icon.gmd_local_dining)
-                                    .color(getResources().getColor(R.color.colorMapIcon))
-                                    .sizeDp(24).toBitmap())));
-                    hamburg.setFlat(true);
-                    mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                        @Override
-                        public boolean onMarkerClick(Marker marker) {
-                            return true;
-                        }
-                    });
-                    // Move the camera instantly to hamburg with a zoom of 15.
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(venueLatLng, 10));
-                    mMap.getUiSettings().setAllGesturesEnabled(false);
-                    // Zoom in, animating the camera.
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(18), 2000, null);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                        Log.d(TAG, "setupVenueMap: " + (mMapFragment.getView() == null));
-                    }
-                }
+        getMapAsync(new OnMapReadyCallback() {
+          @Override public void onMapReady(GoogleMap mMap) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            Marker hamburg = mMap.addMarker(new MarkerOptions().position(venueLatLng)
+                .icon(BitmapDescriptorFactory.fromBitmap(
+                    new IconicsDrawable(AppController.context).icon(
+                        GoogleMaterial.Icon.gmd_local_dining)
+                        .color(getResources().getColor(R.color.colorMapIcon))
+                        .sizeDp(24)
+                        .toBitmap())));
+            hamburg.setFlat(true);
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+              @Override public boolean onMarkerClick(Marker marker) {
+                return true;
+              }
+            });
+            // Move the camera instantly to hamburg with a zoom of 15.
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(venueLatLng, 10));
+            mMap.getUiSettings().setAllGesturesEnabled(false);
+            // Zoom in, animating the camera.
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(18), 2000, null);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+              Log.d(TAG, "setupVenueMap: " + (mMapFragment.getView() == null));
             }
-        };
-        getChildFragmentManager().beginTransaction().add(R.id.venueMap, mMapFragment).commit();
+          }
+        });
+      }
+    };
+    getChildFragmentManager().beginTransaction().add(R.id.venueMap, mMapFragment).commit();
+  }
 
-    }
+  private void setupViews() {
 
-    private void setupViews() {
-
-    }
+  }
 }
--- app/src/main/java/com/ashojash/android/fragment/VenueMiniMapFragment.java
+++ app/src/main/java/com/ashojash/android/fragment/VenueMiniMapFragment.java
@@ -5,6 +5,7 @@ import com.ashojash.android.R;
 import com.ashojash.android.helper.AppController;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -14,52 +15,49 @@ import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
 
 public class VenueMiniMapFragment extends SupportMapFragment {
-    private GoogleMap mMap;
-    public LatLng venueLatLng;
+  public LatLng venueLatLng;
 
-    public VenueMiniMapFragment() {
-    }
+  public VenueMiniMapFragment() {
+  }
 
-    private OnMapClickListener listener;
+  private OnMapClickListener listener;
 
-    public interface OnMapClickListener {
-        void onClick();
-    }
+  public interface OnMapClickListener {
+    void onClick();
+  }
 
-    public void setOnMapClickListener(OnMapClickListener listener) {
-        this.listener = listener;
-    }
+  public void setOnMapClickListener(OnMapClickListener listener) {
+    this.listener = listener;
+  }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mMap = this.getMap();
-        if (mMap != null) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            Marker marker = mMap.addMarker(new MarkerOptions().position(venueLatLng)
-                    .icon(BitmapDescriptorFactory.fromBitmap(new IconicsDrawable(AppController.context)
-                            .icon(GoogleMaterial.Icon.gmd_local_dining)
-                            .color(getResources().getColor(R.color.colorMapIcon))
-                            .sizeDp(24).toBitmap())));
-            marker.setFlat(true);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(venueLatLng, 10));
-            mMap.getUiSettings().setAllGesturesEnabled(false);
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(18), 2000, null);
-            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                @Override
-                public boolean onMarkerClick(Marker marker) {
-                    if (listener != null)
-                        listener.onClick();
-                    return true;
-                }
-            });
-            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                @Override
-                public void onMapClick(LatLng latLng) {
-                    if (listener != null)
-                        listener.onClick();
-                }
-            });
-        }
-    }
+  @Override public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    this.getMapAsync(new OnMapReadyCallback() {
+      @Override public void onMapReady(GoogleMap mMap) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        Marker marker = mMap.addMarker(new MarkerOptions().position(venueLatLng)
+            .icon(BitmapDescriptorFactory.fromBitmap(
+                new IconicsDrawable(AppController.context).icon(
+                    GoogleMaterial.Icon.gmd_local_dining)
+                    .color(getResources().getColor(R.color.colorMapIcon))
+                    .sizeDp(24)
+                    .toBitmap())));
+        marker.setFlat(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(venueLatLng, 10));
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18), 2000, null);
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+          @Override public boolean onMarkerClick(Marker marker) {
+            if (listener != null) listener.onClick();
+            return true;
+          }
+        });
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+          @Override public void onMapClick(LatLng latLng) {
+            if (listener != null) listener.onClick();
+          }
+        });
+      }
+    });
+  }
 }
